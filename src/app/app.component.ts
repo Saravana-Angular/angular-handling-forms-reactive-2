7cc91f403380373d850896739bfb0758658d1886
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-18 10:50:57 -0400

    creating ts form and connecting the ts form with html form and controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormGroupName, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('critical')
+    })
+  }
+  
 }
